fb4acde2cadfb4575474bc46f44a8a89ef23e3c4
Rick in 't veld <rick.in.t.veld@opinity.nl>
2022-08-25 14:12:01 +0200

    Added the dist folder to the gitignore

--- dist/application.js
+++
@@ -1,2 +0,0 @@
-console.log('STARTED!');
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwbGljYXRpb24uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvYXBwbGljYXRpb24udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxDQUFDLEdBQUcsQ0FBQyxVQUFVLENBQUMsQ0FBQyJ9
