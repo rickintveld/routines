5690a926a80c99f9f02ec75240c49f55d0abc04b
Rick in 't veld <rick.in.t.veld@opinity.nl>
2022-08-27 14:42:41 +0200

    adjusted the command handler logger

--- src/Application/CommandHandler/CommandHandler.ts
+++ src/Application/CommandHandler/CommandHandler.ts
@@ -7,16 +7,13 @@ export default abstract class CommandHandler<T> {
     constructor(private logger: Logger, private say: Say) {}
 
     public async handle(command: T): Promise<void> {
-        this.logger.info('Update command state to: Pending');
         await this.updateState(command, 'Pending');
 
         try {
             const output = await this.execute(command);
-            this.logger.info(output);
             this.say.speak(output);
         } catch (e) {
             this.logger.error(e.message);
-            this.logger.info('Update command state to: Failed');
             await this.updateState(command, 'Failed');
         }
 
@@ -28,6 +25,7 @@ export default abstract class CommandHandler<T> {
 
     private async updateState(command: T, state: State): Promise<void> {
         // save command state in redis
+        this.logger.info(`Update command state to: ${state}"`);
     }
 }
 
